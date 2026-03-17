8cc3c638b9155b1d31ce9a7e0d0d8e37e0010273
flciajea <feliciajean197@gmail.com>
2026-03-17 10:46:39 +0700

    Dashboard User
    menambahkan tema di dashboard user, masih berantakan

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
 <h1>Dashboard User</h1>
 
 <p>Selamat datang, {{ Auth::user()->name }}</p>
@@ -6,3 +9,12 @@
 @csrf
 <button type="submit">Logout</button>
 </form>
+@endsection
+
+@section("ExtraCSS")
+@endsection
+
+@section("ExtraJS")
+@endsection 
+
+
